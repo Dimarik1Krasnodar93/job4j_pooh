0a4690f40e7c9721f196e76d17a22e19ac97a591
Dimarik <krasnodar93@mail.ru>
2022-12-06 14:52:53 +0300

    0612

--- src/main/java/ru/job4j/pooh/QueueService.java
+++ src/main/java/ru/job4j/pooh/QueueService.java
@@ -2,25 +2,29 @@ package ru.job4j.pooh;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class QueueService implements Service {
     private final Map<String, Queue<Resp>> map = new ConcurrentHashMap<>();
     private static final String STR_GET = "GET";
+    private static final Resp EMPTY_RESPONSE = new Resp("", "");
 
     public QueueService() {
     }
 
     @Override
     public Resp process(Req req) {
-        if (!map.containsKey(req.getSourceName())) {
-            map.put(req.getSourceName(), new ArrayDeque<Resp>());
-        }
+        Resp result = EMPTY_RESPONSE;
+        map.putIfAbsent(req.getSourceName(), new ConcurrentLinkedQueue<>());
         if (STR_GET.equals(req.httpRequestType())) {
-            return get(req);
+            Resp message = map.get(req.getSourceName()).poll();
+            if (message != null) {
+                result = message;
+            }
         } else {
-            return post(req);
+            map.get(req.getSourceName()).offer(new Resp(req.getSourceName(), req.getParam()));
         }
-
+       return result;
     }
 
     private Resp post(Req req) {
